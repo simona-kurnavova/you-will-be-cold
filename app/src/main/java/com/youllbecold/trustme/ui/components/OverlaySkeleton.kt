1eaa27d786825db3f6aaa3fac151bb629e5d2ee6
Simona Kurňavová <kurnavova.simona@gmail.com>
2025-02-28 17:14:39 +0100

    Adjust paddings and button on overlay screen.

--- app/src/main/java/com/youllbecold/trustme/ui/components/OverlaySkeleton.kt
+++ app/src/main/java/com/youllbecold/trustme/ui/components/OverlaySkeleton.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.youllbecold.trustme.R
+import com.youllbecold.trustme.ui.components.generic.ThemedButton
 import com.youllbecold.trustme.ui.components.utils.center
 import com.youllbecold.trustme.ui.theme.YoullBeColdTheme
 
@@ -79,20 +78,15 @@ fun OverlaySkeleton(
 
         Spacer(modifier = Modifier.height(SPACE_BETWEEN_ITEMS.dp))
 
-        Button(
-            onClick = { action() },
-            colors = ButtonDefaults.buttonColors().copy(
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            ),
-        ) {
-            Text(text = stringResource(buttonText))
-        }
+        ThemedButton(
+            text = stringResource(buttonText),
+            onClick = action,
+        )
     }
 }
 private const val PADDING_HORIZONTAL = 18
 private const val SPACE_UNDER_IMAGE = 24
-private const val SPACE_BETWEEN_ITEMS = 12
+private const val SPACE_BETWEEN_ITEMS = 24
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
